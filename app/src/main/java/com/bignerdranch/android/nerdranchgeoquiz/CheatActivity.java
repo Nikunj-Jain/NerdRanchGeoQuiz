f1356386266aa910a0f8ec3016642469417b9536
Nikunj Jain <nj.nikunjjain@gmail.com>
2017-08-06 23:22:24 +0530

    Chapter 6 inclusive of challenge 1

--- app/src/main/java/com/bignerdranch/android/nerdranchgeoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/nerdranchgeoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.nerdranchgeoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +19,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
+    private TextView mApiVersionTextView;
     private Button mShowAnswerButton;
 
     @Override
@@ -26,6 +31,7 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mApiVersionTextView = (TextView) findViewById(R.id.api_version_text_view);
 
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +42,25 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        mApiVersionTextView.setText("API Level " + Build.VERSION.SDK_INT);
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
